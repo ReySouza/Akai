51fbcf87519c2606938b2ae600ed84d37254cb99
ReySouza <122836752+ReySouza@users.noreply.github.com>
2023-02-10 01:19:38 -0300

    Update characterSheet.js

--- characterSheet.js
+++ characterSheet.js
@@ -8,6 +8,7 @@ const data = {
   points: 10,
   energy: 14,
   season: 12,
+  focus: 10,
 
   life: {
     current: 16,
@@ -245,6 +246,7 @@ $('#birthplace').val(data.birthplace)
 $('#points').val(data.points)
 $('#energy').val(data.energy)
 $('#season').val(data.season)
+$('#focus').val(data.focus)
 
 $('.lifeBar').css('width', `${calculateBar(data.life.current, data.life.max)}%`)
 $('#lifeCount').text(`${data.life.current}/${data.life.max}`)
